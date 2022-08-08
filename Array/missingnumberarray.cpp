b69045ec1ee6662670bb73590166c6266140c2c6
Asheesh Kumar <asheesh220720@gmail.com>
2022-08-08 17:58:49 +0530

    Missing number in array

--- Array/missingnumberarray.cpp
+++ Array/missingnumberarray.cpp
@@ -1,3 +1,5 @@
+1st Approach :
+
 #include <bits/stdc++.h>
 using namespace std;
 class Solution{
@@ -11,6 +13,35 @@ class Solution{
             res=res^i;
         }
         return res;
+    }
+};
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> array(n - 1);
+        for (int i = 0; i < n - 1; ++i) cin >> array[i];
+        Solution obj;
+        cout << obj.MissingNumber(array, n) << "\n";
+    }
+    return 0;
+}
+
+2nd Approach :
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+  public:
+    int MissingNumber(vector<int>& array, int n) {
+       int total=(n*(n+1))/2;
+       int sum=0;
+       for(int i=0;i<n-1;i++){
+           sum+=array[i];
+       }
+       return (total-sum);
     }
 };
 int main() {
